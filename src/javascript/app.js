6faa9c8416fa3eec30dd894513013d6f0582af20
Greaby <hello@greaby.co>
2022-04-02 09:48:50 +0200

    add search

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -4,6 +4,8 @@ import config from "../../config";
 
 const loadSigma = async (json_file) => {
     const container = document.querySelector("#graph-container");
+    const search_input = document.querySelector("input[type=search]");
+    const search_suggestions = document.querySelector("#suggestions");
 
     const data = await fetch(json_file).then((response) => response.json());
 
@@ -17,17 +19,69 @@ const loadSigma = async (json_file) => {
 
     const renderer = new Sigma(graph, container, settings);
 
-    let hoveredNode = undefined;
-    let hoveredNeighbors = undefined;
+    let hovered_node = undefined;
+    let hovered_neighbors = undefined;
+    let selected_node = undefined;
+    let suggestions = undefined;
+
     renderer.on("enterNode", ({ node }) => {
-        hoveredNode = node;
-        hoveredNeighbors = graph.neighbors(node);
+        hovered_node = node;
+        hovered_neighbors = graph.neighbors(node);
         renderer.refresh();
     });
     renderer.on("leaveNode", () => {
-        hoveredNode = undefined;
-        hoveredNeighbors = undefined;
+        hovered_node = undefined;
+        hovered_neighbors = undefined;
         renderer.refresh();
+    });
+
+    const set_search_query = (query) => {
+        if (search_input.value !== query) {
+            search_input.value = query;
+        }
+
+        if (query) {
+            const lc_query = query.toLowerCase();
+            const nodes = graph
+                .nodes()
+                .map((n) => ({
+                    id: n,
+                    label: graph.getNodeAttribute(n, "label"),
+                }))
+                .filter(({ label }) => label.toLowerCase().includes(lc_query));
+
+            // If we have a single perfect match, them we remove the suggestions, and
+            // we consider the user has selected a node through the datalist
+            // autocomplete:
+            if (nodes.length === 1 && nodes[0].label === query) {
+                selected_node = nodes[0].id;
+                suggestions = undefined;
+
+                const node_position =
+                    renderer.getNodeDisplayData(selected_node);
+                renderer.getCamera().animate(node_position, {
+                    duration: 500,
+                });
+            }
+            // Else, we display the suggestions list:
+            else {
+                selected_node = undefined;
+                suggestions = new Set(nodes.map(({ id }) => id));
+            }
+        } else {
+            selected_node = undefined;
+            suggestions = undefined;
+        }
+
+        // Refresh rendering:
+        renderer.refresh();
+    };
+
+    search_input.addEventListener("input", (event) => {
+        set_search_query(event.currentTarget.value || "");
+    });
+    search_input.addEventListener("blur", () => {
+        set_search_query("");
     });
 
     graph.forEachNode((node, attributes) => {
@@ -59,12 +113,35 @@ const loadSigma = async (json_file) => {
         }
     );
 
+    search_suggestions.innerHTML = graph
+        .nodes()
+        .sort(
+            (a, b) =>
+                graph.getNodeAttribute(a, "label") >
+                graph.getNodeAttribute(b, "label")
+        )
+        .map(
+            (node) =>
+                `<option value="${graph.getNodeAttribute(
+                    node,
+                    "label"
+                )}"></option>`
+        )
+        .join("");
+
     renderer.setSetting("nodeReducer", (node, attributes) => {
         if (
-            hoveredNeighbors &&
-            !hoveredNeighbors.includes(node) &&
-            hoveredNode !== node
+            hovered_neighbors &&
+            !hovered_neighbors.includes(node) &&
+            hovered_node !== node
         ) {
+            attributes.label = "";
+            attributes.color = "#f6f6f6";
+        }
+
+        if (selected_node === node) {
+            attributes.highlighted = true;
+        } else if (suggestions && !suggestions.has(node)) {
             attributes.label = "";
             attributes.color = "#f6f6f6";
         }
@@ -73,12 +150,20 @@ const loadSigma = async (json_file) => {
     });
 
     renderer.setSetting("edgeReducer", (edge, attributes) => {
-        if (hoveredNode) {
-            if (!graph.hasExtremity(edge, hoveredNode)) {
+        if (hovered_node) {
+            if (!graph.hasExtremity(edge, hovered_node)) {
                 attributes.hidden = true;
             } else {
                 attributes.size = 3;
             }
+        }
+
+        if (
+            suggestions &&
+            (!suggestions.has(graph.source(edge)) ||
+                !suggestions.has(graph.target(edge)))
+        ) {
+            attributes.hidden = true;
         }
 
         return attributes;
